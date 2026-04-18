4fa5e5b089c22ca1e6d42e0245ee80c470a59fc0
michaelborhlar <michealadefehinti09@gmail.com>
2026-04-18 20:56:52 +0100

    Update Django settings for security and CORS

--- core/settings.py
+++ core/settings.py
@@ -1,31 +1,45 @@
-import os
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("SECRET_KEY", "change-me-in-production-use-env-var")
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-me-in-production')
 
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
-    "django.contrib.contenttypes",
-    "django.contrib.auth",
-    "api",
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
+    'corsheaders',
+    'rest_framework',
+    'profiles',
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "api.middleware.CORSMiddleware",
-    "django.middleware.common.CommonMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
-ROOT_URLCONF = "core.urls"
+ROOT_URLCONF = 'core.urls'
 
-WSGI_APPLICATION = "core.wsgi.application"
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'profiles.db',
+    }
+}
 
-# No database needed
-DATABASES = {}
+# CORS
+CORS_ALLOW_ALL_ORIGINS = True
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+# DRF
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': ['rest_framework.renderers.JSONRenderer'],
+    'DEFAULT_PARSER_CLASSES': ['rest_framework.parsers.JSONParser'],
+}
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+USE_TZ = True
